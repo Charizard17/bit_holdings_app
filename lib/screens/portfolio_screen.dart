51c9dcbe6b083421294fc3d1ab7a711489d6aa66
Esad Dursun <3723esad@gmail.com>
2022-03-27 00:08:21 +0100

    portfolio_screen listview scroll fix

--- lib/screens/portfolio_screen.dart
+++ lib/screens/portfolio_screen.dart
@@ -59,124 +59,155 @@ class PortfolioScreen extends StatelessWidget {
                         double currentValue = 0.0;
 
                         streamSnapshot.data!.docs.forEach((document) {
-                          final Coin tempCoin = futureSnapshot.data
-                              .firstWhere((coin) => coin.name == document.id.replaceAll('-', ' '));
-                          totalInvested +=
-                              double.parse(document.get('Quantity').toString()) *
-                                  double.parse(document.get('Price').toString());
-                          currentValue +=
-                              double.parse(document.get('Quantity').toString()) *
-                                  double.parse(tempCoin.price.toString());
+                          final Coin tempCoin = futureSnapshot.data.firstWhere(
+                              (coin) =>
+                                  coin.name ==
+                                  document.id.replaceAll('-', ' '));
+                          totalInvested += double.parse(
+                                  document.get('Quantity').toString()) *
+                              double.parse(document.get('Price').toString());
+                          currentValue += double.parse(
+                                  document.get('Quantity').toString()) *
+                              double.parse(tempCoin.price.toString());
                         });
 
-                        return Column(
-                          children: [
-                            TotalPortfolioInfo(
-                              totalInvested: totalInvested.toStringAsFixed(2),
-                              currentValue: currentValue.toStringAsFixed(2),
-                            ),
-                            SizedBox(height: 15),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Text(
-                                  'Your Assets',
-                                  style: TextStyle(
-                                    fontSize: 22,
-                                  ),
-                                ),
-                                SizedBox(
-                                  width: 35,
-                                  height: 35,
-                                  child: OpenContainer(
-                                    transitionType:
-                                        ContainerTransitionType.fadeThrough,
-                                    closedColor: Colors.transparent,
-                                    closedElevation: 0.0,
-                                    openElevation: 4.0,
-                                    transitionDuration:
-                                        Duration(milliseconds: 1000),
-                                    openBuilder: (BuildContext context,
-                                            VoidCallback closeContainer) =>
-                                        AddCoinScreen(),
-                                    closedBuilder: (BuildContext _,
-                                        VoidCallback openContainer) {
-                                      return Container(
-                                        decoration: BoxDecoration(
-                                          color: Color(0XFF59B5B2),
-                                          borderRadius:
-                                              BorderRadius.circular(50),
-                                        ),
-                                        child: Icon(Icons.add),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 15),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    'Coin',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                    ),
-                                    textAlign: TextAlign.left,
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    'Holdings',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                    ),
-                                    textAlign: TextAlign.right,
-                                  ),
-                                ),
-                                Expanded(
-                                  flex: 2,
-                                  child: Text(
-                                    'Profit/Loss',
-                                    style: TextStyle(
-                                      fontSize: 18,
-                                    ),
-                                    textAlign: TextAlign.right,
-                                  ),
-                                ),
-                              ],
-                            ),
-                            SizedBox(height: 5),
-                            Divider(color: Colors.white),
-                            ListView(
-                              scrollDirection: Axis.vertical,
-                              shrinkWrap: true,
-                              children:
-                                  streamSnapshot.data!.docs.map((document) {
-                                final Coin coin = futureSnapshot.data
-                                    .firstWhere(
-                                        (coin) => coin.name == document.id.replaceAll('-', ' '));
-                                return Container(
-                                  child: PortfolioListItem(
-                                    name: document.id,
-                                    buyPrice: document['Price'].toString(),
-                                    quantity: document['Quantity'].toString(),
-                                    coin: coin,
-                                  ),
-                                );
-                              }).toList(),
-                            ),
-                          ],
+                        return TotalPortfolioInfo(
+                          totalInvested: totalInvested.toStringAsFixed(2),
+                          currentValue: currentValue.toStringAsFixed(2),
                         );
                       },
                     ),
                   );
                 },
               ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    'Your Assets',
+                    style: TextStyle(
+                      fontSize: 22,
+                    ),
+                  ),
+                  SizedBox(
+                    width: 35,
+                    height: 35,
+                    child: OpenContainer(
+                      transitionType: ContainerTransitionType.fadeThrough,
+                      closedColor: Colors.transparent,
+                      closedElevation: 0.0,
+                      openElevation: 4.0,
+                      transitionDuration: Duration(milliseconds: 1000),
+                      openBuilder:
+                          (BuildContext context, VoidCallback closeContainer) =>
+                              AddCoinScreen(),
+                      closedBuilder:
+                          (BuildContext _, VoidCallback openContainer) {
+                        return Container(
+                          decoration: BoxDecoration(
+                            color: Color(0XFF59B5B2),
+                            borderRadius: BorderRadius.circular(50),
+                          ),
+                          child: Icon(Icons.add),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 15),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Coin',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.left,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Holdings',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                  Expanded(
+                    flex: 2,
+                    child: Text(
+                      'Profit/Loss',
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                      textAlign: TextAlign.right,
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 5),
+              Divider(color: Colors.white),
+              Expanded(
+                child: FutureBuilder(
+                  future: _apiMethods.getCoinsList(),
+                  builder: (BuildContext context, AsyncSnapshot futureSnapshot) {
+                    if (!futureSnapshot.hasData) {
+                      return Center(
+                        child: CircularProgressIndicator(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                      );
+                    }
+              
+                    return Container(
+                      child: StreamBuilder(
+                        stream: FirebaseFirestore.instance
+                            .collection('Users')
+                            .doc(FirebaseAuth.instance.currentUser?.uid)
+                            .collection('Coins')
+                            .snapshots(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                          if (!streamSnapshot.hasData) {
+                            return Center(
+                              child: CircularProgressIndicator(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                            );
+                          }
+              
+                          return ListView(
+                            scrollDirection: Axis.vertical,
+                            shrinkWrap: true,
+                            children: streamSnapshot.data!.docs.map((document) {
+                              final Coin coin = futureSnapshot.data.firstWhere(
+                                  (coin) =>
+                                      coin.name ==
+                                      document.id.replaceAll('-', ' '));
+                              return Container(
+                                child: PortfolioListItem(
+                                  name: document.id,
+                                  buyPrice: document['Price'].toString(),
+                                  quantity: document['Quantity'].toString(),
+                                  coin: coin,
+                                ),
+                              );
+                            }).toList(),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                ),
+              ),
+              SizedBox(height: 10),
             ],
           ),
         ),
